6296409a2d44b9ecdcd20123631ae31ebaee6057
Mohamed Hussein <mowz971@gmail.com>
2020-11-10 23:56:58 +0000

    Setup

---
+++ src/app.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Box, Text } from "@chakra-ui/core";
+
+function App() {
+  return (
+    <Box padding={4}>
+      <Box height="400px">
+        <Text fontSize="50px" color="tomato">
+          Hello World
+        </Text>
+      </Box>
+    </Box>
+  );
+}
+
+export default App;
---
+++ src/readme.md
@@ -0,0 +1,3 @@
+# Whats in the mix?
+
+React (obv) + Chakra UI + React Hooks Form + ...
