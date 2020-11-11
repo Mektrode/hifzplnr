9272aa05ece86b57cbcfee097e97fedf385a3055
Mohamed Hussein <mowz971@gmail.com>
2020-11-11 00:39:51 +0000

    base containers

--- src/app.js
+++ src/app.js
@@ -1,13 +1,48 @@
 import React from "react";
-import { Box, Text } from "@chakra-ui/core";
+import { Box, Text, Flex } from "@chakra-ui/core";
 
 function App() {
   return (
-    <Box padding={4}>
-      <Box height="400px">
-        <Text fontSize="50px" color="tomato">
-          Hello World
-        </Text>
+    <Box>
+      <Box
+        class="container"
+        display="flex"
+        flexDirection="column"
+        height="100vh"
+        alignItems="center"
+        justifyContent="space-around"
+        backgroundColor="teal.100"
+        //border="1px"
+        //borderColor="black"
+      >
+        <Box
+          class="main_content"
+          width="30vw"
+          height="100%"
+          display="flex"
+          flexDirection="column"
+          alignItems="center"
+          justifyContent="flex-start"
+          backgroundColor="white"
+          borderRadius="md"
+          //border="1px"
+          //borderColor="gray"
+        >
+          <Text fontSize="xl" color="teal.300">
+            Hifz Planner
+          </Text>
+          <Text fontSize="m" color="grey">
+            Calculate your estimated Hifz end date
+          </Text>
+          <Box
+            height="1px"
+            width="100%"
+            borderTop="1px"
+            borderColor="teal.100"
+            borderRadius="md"
+            marginY="10px"
+          />
+        </Box>
       </Box>
     </Box>
   );
--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from "react";
 import { render } from "react-dom";
-import { ThemeProvider, theme } from "@chakra-ui/core";
+import { ThemeProvider, theme, CSSReset } from "@chakra-ui/core";
 import App from "./app";
 
 const rootElement = document.getElementById("root");
 render(
   <ThemeProvider theme={theme}>
+    <CSSReset />
     <App />
   </ThemeProvider>,
   rootElement
