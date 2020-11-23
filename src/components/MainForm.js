9f90fb963561ecda2083e2f0f7a6f15d8a95a702
Mohamed Hussein <mowz971@gmail.com>
2020-11-23 15:24:37 +0000

    Two input form controllers to start with

--- src/components/MainForm.js
+++ src/components/MainForm.js
@@ -1,12 +1,67 @@
 import React from "react";
-import { Box, Flex, Text } from "@chakra-ui/core";
+import {
+  Box,
+  Flex,
+  Button,
+  Text,
+  FormControl,
+  Input,
+  Select,
+  FormLabel,
+  FormHelperText,
+  NumberDecrementStepper,
+  NumberIncrementStepper,
+  NumberInput,
+  NumberInputField,
+  NumberInputStepper,
+} from "@chakra-ui/core";
 
-function MainForm () {
-    return (
-        <Flex flexGrow="1">
-            <Text>Form</Text>
-        </Flex>
-    )
+function MainForm() {
+  return (
+    <Flex flexGrow="1" flexDirection="column" alignContent="center">
+      <Box>
+        <Text>Stage 1</Text>
+        <Text>How far are you now?</Text>
+      </Box>
+
+      <Box class="main_form" width="100%">
+        <FormControl id="start_surah">
+          <FormLabel>You memorise from:</FormLabel>
+          <Select placeholder="Select Surah">
+            <option>Nas (Bottom)</option>
+            <option>Baqarah (Top)</option>
+          </Select>
+          <FormHelperText>
+            Please select which surah you started memorising from
+          </FormHelperText>
+        </FormControl>
+
+        <FormControl id="current_page">
+          {/*Should change to mobile friendly version using hooks*/}
+          <FormLabel>Which page are you currently on?</FormLabel>
+          <NumberInput defaultValue={1} min={1} max={600}>
+            <NumberInputField />
+            <NumberInputStepper>
+              <NumberIncrementStepper />
+              <NumberDecrementStepper />
+            </NumberInputStepper>
+          </NumberInput>
+          <FormHelperText>Please input your current page number</FormHelperText>
+        </FormControl>
+      </Box>
+
+      <Box class="form_controls">
+        <Button
+          mt={4}
+          variantColor="teal"
+          //isLoading={formState.isSubmitting}
+          type="submit"
+        >
+          Submit
+        </Button>
+      </Box>
+    </Flex>
+  );
 }
 
 export default MainForm;
